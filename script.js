26032bda8b63e019b8905b183b90479ea08fc4f2
StefanoTesla <92517402+StefanoTesla@users.noreply.github.com>
2022-08-05 13:28:46 +0200

    Create script.js
    Firts commit

---
+++ script.js
@@ -0,0 +1,68 @@
+
+const elements = document.querySelectorAll('[PUT_YOUR_ATTRIBUTE_NAME]');
+const filterDiv = document.querySelector("#PUT_YOUR_CODEBLOCK_ID") //
+let obj = {};
+
+
+console.log(elements);
+elements.forEach(el => {
+  const keys = el.getAttribute('PUT_YOUR_ATTRIBUTE_NAME').split(',');
+  keys.forEach(key => {
+    if(obj[key] === undefined){
+      obj[key] = [];
+    }
+    obj[key].push(el.id)
+  })
+})
+
+Object.keys(obj).forEach(key => {
+            const label = document.createElement('label');
+            label.setAttribute("for", key);
+           
+            // create a checkbox
+            const checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+            checkbox.name = "grapes";
+            checkbox.classList.add("filter-checkbox");
+            checkbox.value = key;
+            checkbox.id = key;
+            checkbox.checked = true;
+
+            // place the checkbox inside a label
+            label.appendChild(checkbox);
+            // create text node
+            label.appendChild(document.createTextNode(key));
+            // add the label to the root
+            filterDiv.appendChild(label);
+        });
+
+
+function filter(){
+    let v = [];
+    let n = [];
+    let a = filterDiv.querySelectorAll(".filter-checkbox");
+    a.forEach(element => {
+            obj[element.id].forEach(divid => {
+                document.querySelector("#" + divid).classList.remove("flt-invisible");
+                if(element.checked){
+                    v.push("#"+divid);
+                } else {
+                    n.push("#"+divid);
+                }
+            })
+    });
+
+    v = new Set(v);
+    v = Array.from(v);
+
+    v = Object.values(v);
+    n = new Set(n);
+    n = Array.from(n);
+
+    let d = n.filter(x => !v.includes(x));
+
+    d.forEach(x => {
+        document.querySelector(x).classList.add("flt-invisible");
+
+    })
+}
